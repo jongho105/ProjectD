6026c5bc94f097570564d4ba229e638b1c4d0b26
sin15348@naver.com <tjwogus97@gmail.com>
2022-11-30 23:21:00 +0900

    ADD : 맵툴 테스트 커스텀 에디터 커밋

---
+++ Assets/Script/Editor/TabWindows.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TabWindows : MonoBehaviour
+{
+    #region Tools
+
+    [MenuItem("Tools/MapTool")]
+    static void OpenMapTool()
+    {
+        Debug.Log("MapTool Open");
+    }
+    #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+}
